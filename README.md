2da3fb12739e7ce8de961afd0b9e1c8c50a084dc
richard <richard>
2019-10-21 16:10:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# malloc
+
+A memory allocator for web assembly.
---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+use core::alloc::{GlobalAlloc, Layout};
+
+#[derive(Copy, Clone)]
+pub struct Allocator;
+
+static mut START:usize = 0;
+
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        self.mem_malloc(layout.size())
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        self.mem_free(ptr as usize)
+    }
+}
+
+impl Allocator {
+    unsafe fn mem_malloc(&self, size: usize) -> *mut u8 {
+        let p = START;
+        START += size;
+        return p as *mut u8;
+    }
+
+    unsafe fn mem_free(&self, _loc: usize) {
+        // TODO
+    }
+}
