a6ce286289ea72583c37aa1338c486a8ade448d4
richard <richard>
2019-11-04 20:13:08 -0800

    failing test

---
+++ examples/global/src/lib.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![feature(alloc_error_handler)]
+extern crate alloc;
+use alloc::string::String;
+use crate::alloc::string::ToString;
+use js_ffi::*;
+use alloc::format;
+
+struct Test {
+    fn_log:f64
+}
+
+impl Default for Test {
+    fn default() -> Self {
+        Test {
+            fn_log:register("console.log")
+        }
+    }
+}
+
+impl Test {
+    fn log(&self, s:&str){
+        call_1(UNDEFINED, self.fn_log, TYPE_STRING, to_js_string(s));
+    }
+}
+
+#[no_mangle]
+pub fn main() -> () {
+    let t = globals::get::<Test>().lock();
+    t.log("hello world")
+}
+
+#[global_allocator]
+static ALLOCATOR:malloc::Allocator = malloc::Allocator;
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[alloc_error_handler]
+fn oom(_: core::alloc::Layout) -> ! {
+    loop {}
+}
